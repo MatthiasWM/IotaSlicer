f347d84e4e7ebaa4c9b86cebda13c3d553915c43
Matthias Melcher <git@matthiasm.com>
2018-08-15 00:33:29 +0200

    Adding potrace source code.

---
+++ potrace/config.h
@@ -0,0 +1,110 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Use the simplified progress bar? */
+/* #undef DUMB_TTY */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Does the C compiler have gcc bug 12243? */
+#define HAVE_GCC_LOOP_BUG /**/
+
+/* Can we use Intel 386 optimizations? */
+#define HAVE_I386 /**/
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Do we have the zlib library? */
+#define HAVE_ZLIB /**/
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of the mkbitmap binary */
+#define MKBITMAP "mkbitmap"
+
+/* Define if missing from math.h */
+/* #undef M_PI */
+
+/* Name of package */
+#define PACKAGE "potrace"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "selinger at users.sourceforge.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "potrace"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "potrace 1.15"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "potrace"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.15"
+
+/* Name of the potrace binary */
+#define POTRACE "potrace"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Use a4 papersize by default? */
+#define USE_A4 /**/
+
+/* Use metric units by default? */
+#define USE_METRIC /**/
+
+/* Version number of package */
+#define VERSION "1.15"
+
+/* Define for Solaris 2.5.1 so the uint64_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT64_T */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Replacement function for strcasecmp */
+/* #undef strcasecmp */
+
+/* Replacement function for strncasecmp */
+/* #undef strncasecmp */
+
+/* Define to the type of an unsigned integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint64_t */
