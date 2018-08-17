0503dcd64a722e8e9f3b73ab532d5b5d82a51aa9
Matthias Melcher <git@matthiasm.com>
2018-08-17 13:31:59 +0200

    Added files that will contain useful little math snacks.

---
+++ src/geometry/IAMath.cpp
@@ -0,0 +1,18 @@
+//
+//  IAMath.cpp
+//
+//  Copyright (c) 2013-2018 Matthias Melcher. All rights reserved.
+//
+
+
+#include "IAMath.h"
+
+
+float min(float a, float b) { return (a<b)?a:b; }
+
+float max(float a, float b) { return (a>b)?a:b; }
+
+double min(double a, double b) { return a<b?a:b; }
+
+double max(double a, double b) { return a>b?a:b; }
+
---
+++ src/geometry/IAMath.h
@@ -0,0 +1,19 @@
+//
+//  IAMath.h
+//
+//  Copyright (c) 2013-2018 Matthias Melcher. All rights reserved.
+//
+
+#ifndef IA_MATH_H
+#define IA_MATH_H
+
+
+extern double min(double a, double b);
+extern double max(double a, double b);
+extern float min(float a, float b);
+extern float max(float a, float b);
+
+
+#endif /* IA_MATH_H */
+
+
